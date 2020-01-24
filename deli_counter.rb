2827c7724cce35ea56fcaac955f14191609377b3
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-24 19:46:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
     puts "The line is currently empty."
     return
   end
-  string_result = "The line is currently: "
+  string_result = "The line is currently:"
   katz_deli.each_with_index{ |customer, position|
     string_result += "#{position + 1}. #{customer}"
   }
@@ -12,5 +12,5 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "#{name} is in position #{katz_deli.size} "
+  puts " #{name} is in position #{katz_deli.size}"
 end
