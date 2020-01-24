7761398e7aef06fd681361d4876f7e9da0bdc974
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-24 19:42:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,15 @@
 # Write your code here.
 def line(katz_deli)
-  if (katz_deli.size = 0)
+  if (katz_deli.size = 0) do
     puts "The line is currently empty."
+    return
   end
+  string_result = ""
+  katz_deli.each{ |customer|
+    puts
+  }
 end
 
 def take_a_number(katz_deli, name)
-  puts "#{name} is in position #{position}"
+  puts "#{name} is in position #{katz_deli.size}"
 end
