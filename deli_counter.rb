4aef4ad698c29a925c52d6b9b33e55a1089cd5ef
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-24 19:40:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(katz_deli)
+  if (katz_deli.size = 0)
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  puts "#{name} is in position #{position}"
+end
